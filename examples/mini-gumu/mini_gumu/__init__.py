c0466ff1692f024c24c5a2a3a08d10c6b0887517
chagumu-01 <3386314071@qq.com>
2026-07-12 23:00:37 +0800

    feat: add mini gumu teaching harness

---
+++ examples/mini-gumu/mini_gumu/__init__.py
@@ -0,0 +1,12 @@
+﻿from .providers import FakeModelClient
+from .runtime import Gumu
+from .state import RunStore, TaskState
+from .workspace import Workspace
+
+__all__ = [
+    "FakeModelClient",
+    "Gumu",
+    "RunStore",
+    "TaskState",
+    "Workspace",
+]
