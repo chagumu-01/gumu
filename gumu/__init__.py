565326dcf4bea748f09ca884c2df0f433c4f5795
chagumu-01 <3386314071@qq.com>
2026-07-12 23:00:16 +0800

    feat: expose package entrypoints

---
+++ gumu/__init__.py
@@ -0,0 +1,18 @@
+from .cli import build_agent, build_arg_parser, build_welcome, main
+from .providers.clients import AnthropicCompatibleModelClient, FakeModelClient, OllamaModelClient, OpenAICompatibleModelClient
+from .runtime import Gumu, SessionStore
+from .workspace import WorkspaceContext
+
+__all__ = [
+    "AnthropicCompatibleModelClient",
+    "FakeModelClient",
+    "Gumu",
+    "build_agent",
+    "build_arg_parser",
+    "build_welcome",
+    "main",
+    "OllamaModelClient",
+    "OpenAICompatibleModelClient",
+    "SessionStore",
+    "WorkspaceContext",
+]
